61bcad61a3e86d8817c912a8db71776fd85c91da
Yaroslav Karandashev <yarix7@gmail.com>
2013-03-24 00:01:13 +0400

    Add price formatting.

--- src/internationalization/PriceCalculator.java
+++ src/internationalization/PriceCalculator.java
@@ -1,10 +1,13 @@
 package internationalization;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class PriceCalculator {
 
     private float orangePrice;
     private int orangeNum;
+    private NumberFormat numberFormatter;
 
     public void setOneOrangePrice(float price) {
         orangePrice = price;
@@ -16,6 +19,14 @@ public class PriceCalculator {
 
     public float calculatePrice() {
         return orangePrice * orangeNum;
+    }
+
+    public String formatPrice() {
+        return numberFormatter.format(calculatePrice());
+    }
+
+    public void setNumberFormatter(Locale locale) {
+        numberFormatter = NumberFormat.getCurrencyInstance(locale);
     }
 
 }
