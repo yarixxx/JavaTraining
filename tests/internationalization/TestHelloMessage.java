66aabff7b14d5faa286f1f358a56fd8a8f1abfe1
Yaroslav Karandashev <kdx836@motorola.com>
2013-03-24 11:47:09 +0400

    add hello test

--- tests/internationalization/TestHelloMessage.java
+++ tests/internationalization/TestHelloMessage.java
@@ -8,11 +8,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestHelloMessage {
-
     private HelloMessage helloMessage;
     private String currentLanguage;
     private String currentCountry;
     private Locale actualLocale;
+    private Object helloString;
+
+    private final static String FR_HELLO = "Bonjour";
+    private final static String EN_HELLO = "Hello";
+    private final static String DE_HELLO = "Hallo";
 
     @Before
     public void setUp() throws Exception {
@@ -25,6 +29,13 @@ public class TestHelloMessage {
         whenGetLocaleCalled();
         thenGermanLocaleReceived();
     }
+    
+    @Test
+    public void testGetFrenchLocale() {
+        givenFrenchLocaleArguments();
+        whenGetLocaleCalled();
+        thenFrenchLocaleReceived();
+    }
 
     @Test
     public void testGetUnknownLocale() {
@@ -32,15 +43,50 @@ public class TestHelloMessage {
         whenGetLocaleCalled();
         thenDefaultLocaleReceived();
     }
+    
+    @Test
+    public void testSayHelloInFrench() {
+        givenFrenchLocaleArguments();
+        whenSayHelloCalled();
+        thenHelloInLocaleReceived(FR_HELLO);
+    }
+    
+    @Test
+    public void testSayHelloInDefault() {
+        givenUnknownLocaleArguments();
+        whenSayHelloCalled();
+        thenHelloInLocaleReceived(EN_HELLO);
+    }
+    
+    @Test
+    public void testSayHelloInGerman() {
+        givenGermanLocaleArguments();
+        whenSayHelloCalled();
+        thenHelloInLocaleReceived(DE_HELLO);
+    }
 
-    private void givenGermanLocaleArguments() {
+    private void thenHelloInLocaleReceived(String expected) {
+        assertEquals(expected, helloString);
+    }
+
+	private void givenGermanLocaleArguments() {
         currentLanguage = "de";
         currentCountry = "DE";
+    }
+
+    private void givenFrenchLocaleArguments() {
+        currentLanguage = "fr";
+        currentCountry = "CA";
     }
 
     private void givenUnknownLocaleArguments() {
         currentLanguage = null;
         currentCountry = null;
+    }
+
+    private void whenSayHelloCalled() {
+        Locale locale = helloMessage.getLocale(currentLanguage, currentCountry);
+        helloString = helloMessage.sayHello(locale);
     }
 
     private void whenGetLocaleCalled() {
@@ -49,6 +95,11 @@ public class TestHelloMessage {
 
     private void thenGermanLocaleReceived() {
         Locale expectedLocale = new Locale("de", "DE");
+        assertEquals(expectedLocale, actualLocale);
+    }
+    
+    private void thenFrenchLocaleReceived() {
+        Locale expectedLocale = new Locale("fr", "CA");
         assertEquals(expectedLocale, actualLocale);
     }
 
