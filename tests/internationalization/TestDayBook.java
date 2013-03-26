bed94889e22c210d59a516797b5c006a46e97206
Yaroslav Karandashev <yarix7@gmail.com>
2013-03-27 00:47:27 +0400

    Add two test cases

--- tests/internationalization/TestDayBook.java
+++ tests/internationalization/TestDayBook.java
@@ -27,10 +27,24 @@ public class TestDayBook {
     }
 
     @Test
+    public void testSetAndGetAnotherStartDay() {
+        givenAnotherStartDay();
+        whenSetStartDay();
+        thenGetStartDayIsTheSame();
+    }
+
+    @Test
     public void testSetAndGetStartDayLocalized() {
         givenStartDay();
         whenSetStartDay();
         thenGetStartDayLocalized();
+    }
+
+    @Test
+    public void testSetAndGetAnotherStartDayLocalized() {
+        givenAnotherStartDay();
+        whenSetStartDay();
+        thenGetStartDayLocalizedInFrench();
     }
 
     private void givenStartDay() {
@@ -38,6 +52,13 @@ public class TestDayBook {
         startDay.set(Calendar.YEAR, 2012);
         startDay.set(Calendar.MONTH, 2);
         startDay.set(Calendar.DAY_OF_MONTH, 24);
+    }
+
+    private void givenAnotherStartDay() {
+        startDay = new GregorianCalendar();
+        startDay.set(Calendar.YEAR, 2011);
+        startDay.set(Calendar.MONTH, 3);
+        startDay.set(Calendar.DAY_OF_MONTH, 21);
     }
 
     private void whenSetStartDay() {
@@ -51,5 +72,10 @@ public class TestDayBook {
     private void thenGetStartDayLocalized() {
         Locale locale = new Locale("en", "US");
         assertEquals("24 Mar 12", dayBook.getStartDay(locale));
+    }
+
+    private void thenGetStartDayLocalizedInFrench() {
+        Locale locale = new Locale("fr", "FR");
+        assertEquals("21 avr. 11", dayBook.getStartDay(locale));
     }
 }
