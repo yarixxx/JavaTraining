47f45be4a702f17f002c30ce1aaae6f94c282c79
Yaroslav Karandashev <yarix7@gmail.com>
2013-04-14 12:38:23 +0400

    Add JUnit test cases for setting and getting Stations.

---
+++ tests/problems3/TestStationImpl.java
@@ -0,0 +1,30 @@
+package problems3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestStationImpl {
+
+    private Station station;
+    private static final String STATION1 = "Девяткино";
+    private static final String STATION2 = "Невский проспект";
+
+    @Before
+    public void setUp() throws Exception {
+        station = new StationImpl();
+    }
+
+    @Test
+    public void testSetGetTitleCase1() {
+        station.setTitle(STATION1);
+        assertEquals(STATION1, station.getTitle());
+    }
+
+    @Test
+    public void testSetGetTitleCase2() {
+        station.setTitle(STATION2);
+        assertEquals(STATION2, station.getTitle());
+    }
+}
