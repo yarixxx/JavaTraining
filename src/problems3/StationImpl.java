03da6456452d919dcbadf01d41a3ce1e87dfb679
Yaroslav Karandashev <yarix7@gmail.com>
2013-04-14 12:35:47 +0400

    Add basic structure for Station class

---
+++ src/problems3/StationImpl.java
@@ -0,0 +1,17 @@
+package problems3;
+
+public class StationImpl implements Station {
+
+    @Override
+    public void setTitle(String title) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
