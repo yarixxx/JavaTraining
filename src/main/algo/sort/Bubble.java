3cea7f8fe168723493fa84bcbb4f4f0064afc73b
Yaroslav Karandashev <yarix7@gmail.com>
2014-05-08 15:14:42 +0400

    Update Bubble.java

--- src/main/algo/sort/Bubble.java
+++ src/main/algo/sort/Bubble.java
@@ -2,16 +2,15 @@ package algo.sort;
 
 public class Bubble extends ArrayOperations implements Sort {
     public int[] sort(int[] array) {
-        boolean continueCycle = true, swapped = true;
+        boolean swapped = true;
         int cursor = 0;
-        while (continueCycle) {
-            if (array[cursor] > array[cursor+1]) {
-                swap(array, cursor, cursor+1);
+        while (swapped || array.length-1 > cursor) {
+            cursor++;
+            if (array[cursor-1] > array[cursor]) {
+                swap(array, cursor-1, cursor);
                 swapped = true;
             }
-            cursor++;
-            if (cursor+2 > array.length) {
-                continueCycle = swapped;
+            if (swapped && array.length-1 <= cursor) {
                 swapped = false;
                 cursor = 0;
             }
